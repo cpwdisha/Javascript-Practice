3c7f51067a2b24e201192f14a90c780f2648cb2a
Disha <disha060303@gmail.com>
2024-06-27 02:43:06 +0530

    Template Literal

---
+++ templateLiteral.js
@@ -0,0 +1,6 @@
+let a =10;
+console.log("value of a is: " + a); // converts to string
+console.log("value of a is: " , a); // a is in integer format
+console.log(`the value of a is ${a}`); // converts full to string
+
+// ways of printing variables
