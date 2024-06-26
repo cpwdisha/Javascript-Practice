b8b8e01d556f338c8624c14706a84bffde33e76f
Disha <disha060303@gmail.com>
2024-06-27 02:41:45 +0530

    For Of Loop

---
+++ forOf.js
@@ -0,0 +1,14 @@
+let arr = [10,20,3,4,5,6];
+
+for(let i of arr)
+    {
+        console.log(arr[i]);
+    }
+
+let str = "Disha Jyotir";
+for(let i of str)
+    {
+        console.log(i);
+    }
+
+// can be used for any iterable object
